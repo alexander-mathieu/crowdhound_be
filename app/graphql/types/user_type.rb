4534358c205dc93d7d53e770c415af8fc7e888d6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-02 14:03:12 -0600

    Add fields to DogType & UserType
    Co-authored-by: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,11 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :email, String, null: true
+    field :short_desc, String, null: true
+    field :long_desc, String, null: true
+    field :dogs, [Types::DogType], null: true
   end
 end
