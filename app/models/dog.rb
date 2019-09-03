9d2be7bfbea31799f4595cca74e82cf77c588599
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-03 16:09:26 -0600

    Add Dog model .age instance method and spec

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,8 @@ class Dog < ApplicationRecord
 
   validates_presence_of :name, :breed, :birthdate, :weight, :activity_level
   validates :activity_level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2 }
+
+  def age
+    ((Time.zone.now - birthdate.to_time) / 1.year.seconds)
+  end
 end
