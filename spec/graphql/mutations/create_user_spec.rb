8882ac8199776e11ed6f092c8797a14c51b9f697
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-03 17:39:02 -0600

    Refactor test of createUser mutation

--- spec/graphql/mutations/create_user_spec.rb
+++ spec/graphql/mutations/create_user_spec.rb
@@ -8,7 +8,21 @@ RSpec.describe "createUser mutation", type: :request do
     short_desc = "my name is bob!"
     long_desc = "ldjflskjfdalsdjfalskjdflsjkdflkjsd sfd sklajdfalsdkjf a dsfalsj. asd fjladskjfa."
 
-    post '/graphql', params: { query: query(first_name: first_name, last_name: last_name, email: email, short_desc: short_desc, long_desc: long_desc) }
+    query = "
+    mutation {
+      createUser(
+        firstName: \"#{first_name}\",
+        lastName: \"#{last_name}\",
+        email: \"#{email}\",
+        shortDesc: \"#{short_desc}\",
+        longDesc: \"#{long_desc}\"
+      ) {
+        #{user_type_attributes}
+      }
+    }
+    "
+
+    post '/graphql', params: { query: query }
     json = JSON.parse(response.body)
 
     data = json['data']['createUser']
@@ -32,20 +46,4 @@ RSpec.describe "createUser mutation", type: :request do
   #   expect { post '/graphql', params: { query: query(id: user.id + 1) } }
   #   .to raise_error(ActiveRecord::RecordNotFound, "Couldn't find User with 'id'=#{user.id + 1}")
   # end
-
-  def query(first_name:, last_name:, email:, short_desc: nil, long_desc: nil)
-    <<~GQL
-      mutation {
-        createUser(
-          firstName: \"#{first_name}\",
-          lastName: \"#{last_name}\",
-          email: \"#{email}\",
-          shortDesc: \"#{short_desc}\",
-          longDesc: \"#{long_desc}\"
-        ) {
-          #{user_type_attributes}
-        }
-      }
-    GQL
-  end
 end
