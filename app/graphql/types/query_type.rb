755e9d7aeaf952ba5e365b8c6da87e06e674bc12
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-03 16:11:08 -0600

    Add filters for activity_level_range, age_range, breed and weight_rannge to dogs query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,14 +2,14 @@ module Types
   class QueryType < Types::BaseObject
     # User Queries
     field :users, [Types::UserType], null: false,
-      description: "Return all users"
+      description: 'Return all users'
 
     def users
       User.all
     end
 
     field :user, Types::UserType, null: false,
-      description: "Find a user by id" do
+      description: 'Find a user by ID' do
       argument :id, ID, required: true
     end
 
@@ -19,14 +19,34 @@ module Types
 
     # Dog Queries
     field :dogs, [Types::DogType], null: false,
-      description: "Return all dogs"
+      description: 'Return all dogs, or all dogs filtered by attribute' do
+        argument :activity_level_range, [Int], required: false
+        argument :age_range, [Float], required: false
+        argument :breed, [String], required: false
+        argument :weight_range, [Int], required: false
+      end
 
-    def dogs
-      Dog.all
+    def dogs(**filters)
+      if filters[:age_range]
+        filters[:birthdate] = (Time.zone.now - (filters[:age_range][1] * 1.year.seconds) - 1.years.seconds)..(Time.zone.now - (filters[:age_range][0] * 1.year.seconds))
+        filters.except!(:age_range)
+      end
+
+      if filters[:activity_level_range]
+        filters[:activity_level] = filters[:activity_level_range][0]..filters[:activity_level_range][1]
+        filters.except!(:activity_level_range)
+      end
+
+      if filters[:weight_range]
+        filters[:weight] = filters[:weight_range][0]..filters[:weight_range][1]
+        filters.except!(:weight_range)
+      end
+
+      Dog.where(filters)
     end
 
     field :dog, Types::DogType, null: false,
-      description: "Find a dog by id" do
+      description: 'Find a dog by ID' do
       argument :id, ID, required: true
     end
 
