db37550142560544c8bb833626ae7fb4586677db
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-04 16:02:11 -0600

    Add Location zip_code validation with spec

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Location, type: :model do
+  describe 'relationships' do
+    it { should belong_to :user }
+  end
+
+  describe 'validations' do
+    it { should validates_presence_of :zip_code }
+  end
+end
