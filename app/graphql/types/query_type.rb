a7a2704e4ef9782b2cf077a14f69ad38ca91d18d
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-03 18:18:46 -0600

    Add exceptions to filter conditional statements

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -28,17 +28,20 @@ module Types
 
     def dogs(**filters)
       if filters[:age_range]
-        filters[:birthdate] = (Time.zone.now - (filters[:age_range][1] * 1.year.seconds) - 1.years.seconds)..(Time.zone.now - (filters[:age_range][0] * 1.year.seconds))
+        raise 'Please provide an array with two integers or floating point numbers.' unless filters[:age_range].count == 2
+        filters[:birthdate] = (Time.zone.now - (filters[:age_range].max * 1.year.seconds) - 1.years.seconds)..(Time.zone.now - (filters[:age_range].min * 1.year.seconds))
         filters.except!(:age_range)
       end
 
       if filters[:activity_level_range]
-        filters[:activity_level] = filters[:activity_level_range][0]..filters[:activity_level_range][1]
+        raise 'Please provide an array with two integers.' unless filters[:activity_level_range].count == 2
+        filters[:activity_level] = filters[:activity_level_range].min..filters[:activity_level_range].max
         filters.except!(:activity_level_range)
       end
 
       if filters[:weight_range]
-        filters[:weight] = filters[:weight_range][0]..filters[:weight_range][1]
+        raise 'Please provide an array with two integers.' unless filters[:weight_range].count == 2
+        filters[:weight] = filters[:weight_range].min..filters[:weight_range].max
         filters.except!(:weight_range)
       end
 
