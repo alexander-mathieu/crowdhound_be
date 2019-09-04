b5e7211e73ac59d13371a6317ea0e41d1c9d2f51
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-04 16:49:56 -0600

    Update user/users query specs to include LocationType

--- spec/graphql/queries/user_spec.rb
+++ spec/graphql/queries/user_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "user query", type: :request do
+RSpec.describe 'user query', type: :request do
   it 'returns a user by id' do
     user = create(:user)
     dogs = create_list(:dog, 2, user: user)
     create(:photo, photoable: dogs.first)
     photo = create(:photo, photoable: user)
+    location = create(:location, user: user)
 
     post '/graphql', params: { query: query(id: user.id) }
 
@@ -23,6 +24,9 @@ RSpec.describe "user query", type: :request do
 
     first_gql_photo = data[:photos].first
     compare_gql_and_db_photos(first_gql_photo, photo)
+
+    gql_location = data[:location]
+    compare_gql_and_db_locations(gql_location, location)
   end
 
   it 'has an error if no user with that id' do
@@ -33,7 +37,7 @@ RSpec.describe "user query", type: :request do
 
     error_message = json[:errors][0][:message]
     expect(error_message).to eq("Couldn't find User with 'id'=#{user.id + 1}")
-    
+
     data = json[:data]
     expect(data).to be_nil
   end
@@ -48,6 +52,9 @@ RSpec.describe "user query", type: :request do
           }
           photos {
             #{photo_type_attributes}
+          }
+          location {
+            #{location_type_attributes}
           }
         }
       }
