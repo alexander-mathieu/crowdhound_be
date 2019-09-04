fb6a2d5a85fe46fdf7f1889ad2d6788700434fb5
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-04 16:30:31 -0600

    Update Location factory

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :location do
     user
-    street_address { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    zip_code { "MyString" }
-    lat { 1.5 }
-    long { 1.5 }
+    street_address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip_code { Faker::Address.zip }
+    lat { Faker::Address.latitude }
+    long { Faker::Address.longitude }
   end
 end
