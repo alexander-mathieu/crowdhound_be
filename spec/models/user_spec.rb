623d545789208aecd91575bf3ab53aa3a345be46
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-05 16:16:23 -0600

    Add User::sorted_by_distance with spec
    Co-Authored-By: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,4 +40,61 @@ RSpec.describe User, type: :model do
       expect(user_instance.distance_to(dog_instance)).to be_within(0.05).of(0.22)
     end
   end
+
+  describe 'class methods' do
+    it '.sorted_by_distance' do
+      VCR.use_cassette('user_model_spec/sorted_by_distance') do
+        user_instance = create(:user)
+
+        Location.create!(
+          user: user_instance,
+          street_address: '1331 17th Street',
+          city: 'Denver',
+          state: 'CO',
+          zip_code: '80202'
+        )
+
+        u1 = create(:user) # 0.89 miles away
+
+        Location.create!(
+          user: u1,
+          street_address: '494 East 19th Avenue',
+          city: 'Denver',
+          state: 'CO',
+          zip_code: '80203'
+        )
+
+        u2 = create(:user) # 16.46 miles away
+
+        Location.create!(
+          user: u2,
+          street_address: '15330 East 120th Place',
+          city: 'Commerce City',
+          state: 'CO',
+          zip_code: '80022'
+        )
+
+        u3 = create(:user) # 2.85 miles away
+
+        Location.create!(
+          user: u3,
+          street_address: '2001 Colorado Boulevard',
+          city: 'Denver',
+          state: 'CO',
+          zip_code: '80205'
+        )
+
+        sorted = User.sorted_by_distance(user_instance)
+
+        expect(sorted[0]).to eq(u1)
+        expect(sorted[0].distance).to be_within(0.05).of(0.89)
+
+        expect(sorted[1]).to eq(u3)
+        expect(sorted[1].distance).to be_within(0.05).of(2.85)
+
+        expect(sorted[2]).to eq(u2)
+        expect(sorted[2].distance).to be_within(0.05).of(16.46)
+      end
+    end
+  end
 end
