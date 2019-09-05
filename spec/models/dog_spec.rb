1d018c94b0f78cb334e1df066db1e54061a355bb
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-05 14:05:30 -0600

    Add test of Dog#distance_to

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -29,5 +29,28 @@ RSpec.describe Dog, type: :model do
     it '#age' do
       expect(@dog.age).to eq(7.000828216869614)
     end
+    
+    it '#distance_to calculates the distance to a user in miles' do
+      current_user = create(:user)
+      create(:location, user: current_user, lat: 39.75113810000001, long: -104.996928)
+      dog = create(:dog, user: current_user)
+      
+      other_user = create(:user)
+      create(:location, user: other_user, lat: 39.7532, long: -105.0002)
+  
+      expect(dog.distance_to(other_user)).to be_within(0.05).of(0.22)
+    end
+  
+    it '#distance_to calculates the distance to a dog in miles' do
+      current_user = create(:user)
+      create(:location, user: current_user, lat: 39.75113810000001, long: -104.996928)
+      current_dog = create(:dog, user: current_user)
+  
+      other_user = create(:user)
+      create(:location, user: other_user, lat: 39.7532, long: -105.0002)
+      other_dog = create(:dog, user: other_user)
+  
+      expect(current_dog.distance_to(other_dog)).to be_within(0.05).of(0.22)
+    end
   end
 end
