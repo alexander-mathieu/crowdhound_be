bb3171bc3b94450b02589d55787440bcadd498aa
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-04 21:48:30 -0600

    Add spec for Location model update_latlong hook

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -8,4 +8,31 @@ RSpec.describe Location, type: :model do
   describe 'validations' do
     it { should validate_presence_of :zip_code }
   end
+
+  describe 'instance methods' do
+    it '#update_latlong' do
+      user = create(:user)
+
+      location = create(:location,
+        user: user,
+        street_address: '1331 17th Street',
+        city: 'Denver',
+        state: 'CO',
+        zip_code: '80202'
+      )
+
+      expect(location.lat).to be_within(0.001).of(39.751138)
+      expect(location.long).to be_within(0.001).of(-104.996928)
+
+      location.update(
+        street_address: '15330 East 120th Place',
+        city: 'Commerce City',
+        state: 'CO',
+        zip_code: '80022'
+      )
+
+      expect(location.lat).to be_within(0.001).of(39.915366)
+      expect(location.long).to be_within(0.001).of(-104.808407)
+    end
+  end
 end
