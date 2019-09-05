cf6af88cf571e5ee7568eb5052ab5b911c159895
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-05 13:33:19 -0600

    Add VCR wraps to Location before_save specs

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -13,13 +13,15 @@ RSpec.describe Location, type: :model do
     before :each do
       user = create(:user)
 
-      @location = Location.create!(
-        user: user,
-        street_address: '1331 17th Street',
-        city: 'Denver',
-        state: 'CO',
-        zip_code: '80202'
-      )
+      VCR.use_cassette('before_save_location_hook_create') do
+        @location = Location.create!(
+          user: user,
+          street_address: '1331 17th Street',
+          city: 'Denver',
+          state: 'CO',
+          zip_code: '80202'
+        )
+      end
     end
 
     it 'updates lat/long on model create' do
@@ -28,15 +30,17 @@ RSpec.describe Location, type: :model do
     end
 
     it 'updates lat/long on model update' do
-      @location.update(
-        street_address: '15330 East 120th Place',
-        city: 'Commerce City',
-        state: 'CO',
-        zip_code: '80022'
-      )
+      VCR.use_cassette('before_save_location_hook_update') do
+        @location.update(
+          street_address: '15330 East 120th Place',
+          city: 'Commerce City',
+          state: 'CO',
+          zip_code: '80022'
+        )
+      end
 
-      expect(location.lat).to be_within(0.001).of(39.915366)
-      expect(location.long).to be_within(0.001).of(-104.808407)
+      expect(@location.lat).to be_within(0.001).of(39.915366)
+      expect(@location.long).to be_within(0.001).of(-104.769292)
     end
   end
 end
