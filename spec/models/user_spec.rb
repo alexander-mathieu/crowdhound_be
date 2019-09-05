5fe950fdc3eea204c7e79d53df976696b6d8de0c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-05 13:31:23 -0600

    Add tests of User#distance_to

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  describe 'instance methods' do
+    it '#distance_to calculates the distance to a user in miles' do
+      current_user = create(:user)
+      create(:location, user: current_user, lat: 39.75113810000001, long: -104.996928)
+
+      other_user = create(:user)
+      create(:location, user: other_user, lat: 39.7532, long: -105.0002)
+
+      expect(current_user.distance_to(other_user)).to be_within(0.05).of(0.22) # 0.22 miles = 0.36 km
+    end
+
+    it '#distance_to calculates the distance to a dog in miles' do
+      current_user = create(:user)
+      create(:location, user: current_user, lat: 39.75113810000001, long: -104.996928)
+
+      other_user = create(:user)
+      create(:location, user: other_user, lat: 39.7532, long: -105.0002)
+      dog = create(:dog, user: other_user)
+
+      expect(current_user.distance_to(dog)).to be_within(0.05).of(0.22) # 0.22 miles = 0.36 km
+    end
+  end
 end
