7b41b26b3b029ec562fff5b857d0601dbea39eb7
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-07 10:16:55 -0600

    Remove createUser mutation, spec and references

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :createUser, mutation: Mutations::CreateUser # TODO remove
     field :authenticate_user, mutation: Mutations::AuthenticateUser
   end
 end
