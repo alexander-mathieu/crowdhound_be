3ccaa77185e2f2f282d8dea9ddf8294fee2a57ae
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-08 16:15:33 -0600

    Fix test descriptions for createDog mutation

--- spec/graphql/mutations/create_dog_spec.rb
+++ spec/graphql/mutations/create_dog_spec.rb
@@ -1,62 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe 'createDog mutation', type: :request do
-  it 'logs out a user that is logged in' do
-    user = create(:user)
-
-    dog_template = Dog.new(
-      name: 'Fluffy',
-      activity_level: 1,
-      weight: 10,
-      breed: 'Miniature Poodle',
-      birthdate: '2018/08/01',
-      short_desc: 'Playful little pup',
-      long_desc: 'What else could you possibly want to know?'
-    )
-
-    params = {
-      google_token: user.google_token,
-      query: create_dog_mutation(dog_template)
-    }
-
-    post '/graphql', params: params
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    gql_dog = json[:data][:createDog][:dog]
-
-    compare_gql_and_db_dogs(gql_dog, dog_template, false)
-
-    expect(user.dogs.count).to eq(1)
+  describe 'as an authenticated user' do
+    it 'creates a dog for the current_user' do
+      user = create(:user)
+  
+      dog_template = Dog.new(
+        name: 'Fluffy',
+        activity_level: 1,
+        weight: 10,
+        breed: 'Miniature Poodle',
+        birthdate: '2018/08/01',
+        short_desc: 'Playful little pup',
+        long_desc: 'What else could you possibly want to know?'
+      )
+  
+      params = {
+        google_token: user.google_token,
+        query: create_dog_mutation(dog_template)
+      }
+  
+      post '/graphql', params: params
+  
+      json = JSON.parse(response.body, symbolize_names: true)
+  
+      gql_dog = json[:data][:createDog][:dog]
+  
+      compare_gql_and_db_dogs(gql_dog, dog_template, false)
+  
+      expect(user.dogs.count).to eq(1)
+    end
   end
 
-  it 'does not create a dog if the request is not authenticated' do
-    dog_template = Dog.new(
-      name: 'Fluffy',
-      activity_level: 1,
-      weight: 10,
-      breed: 'Miniature Poodle',
-      birthdate: '2018/08/01',
-      short_desc: 'Playful little pup',
-      long_desc: 'What else could you possibly want to know?'
-    )
-
-    params = {
-      google_token: 'not a real google token',
-      query: create_dog_mutation(dog_template)
-    }
-
-    post '/graphql', params: params
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    data = json[:data][:createDog]
-    error_message = json[:errors][0][:message]
-
-    expect(data).to be_nil
-    expect(error_message).to eq('Unauthorized - a valid google_token query parameter is required')
-
-    expect(Dog.count).to eq(0)
+  describe 'as a visitor (not authenticated)' do
+    it 'does not create a dog' do
+      dog_template = Dog.new(
+        name: 'Fluffy',
+        activity_level: 1,
+        weight: 10,
+        breed: 'Miniature Poodle',
+        birthdate: '2018/08/01',
+        short_desc: 'Playful little pup',
+        long_desc: 'What else could you possibly want to know?'
+      )
+  
+      params = {
+        google_token: 'not a real google token',
+        query: create_dog_mutation(dog_template)
+      }
+  
+      post '/graphql', params: params
+  
+      json = JSON.parse(response.body, symbolize_names: true)
+  
+      data = json[:data][:createDog]
+      error_message = json[:errors][0][:message]
+  
+      expect(data).to be_nil
+      expect(error_message).to eq('Unauthorized - a valid google_token query parameter is required')
+  
+      expect(Dog.count).to eq(0)
+    end
   end
 
   def create_dog_mutation(dog_template)
