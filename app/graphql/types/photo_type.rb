060989e65ee8f9137f5df35f6a32949f849fc8cd
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-08 17:04:53 -0600

    Add PhotoInputType

--- app/graphql/types/photo_type.rb
+++ app/graphql/types/photo_type.rb
@@ -2,7 +2,7 @@ module Types
   class PhotoType < Types::BaseObject
     field :id, ID, null: false
     field :photoable_id, Int, null: true
-    field :photoable_type, String, null: true
+    field :photoable_type, String, null: true # TODO: use enum instead
     field :source_url, String, null: true
   end
 end
