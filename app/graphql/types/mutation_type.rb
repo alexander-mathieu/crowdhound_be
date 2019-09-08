245e9c611f08780cb0d4361323902db75e950fb2
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-07 18:47:22 -0600

    Add addLocation mutation to GraphQL schema

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :authenticate_user, mutation: Mutations::AuthenticateUser
+    field :add_location, mutation: Mutations::AddLocation
   end
 end
