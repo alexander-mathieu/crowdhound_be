94e3631cc7b8ce5616061eb75602ce8c102a323e
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-08 09:06:20 -0600

    Refactor addLocation mutation spec, add additional spec for invalid location

--- spec/graphql/mutations/add_location_spec.rb
+++ spec/graphql/mutations/add_location_spec.rb
@@ -5,42 +5,90 @@ RSpec.describe 'add location mutation', type: :request do
     @existing_user = create(:user)
   end
 
-  it 'adds a location when a valid Google token is passed in' do
+  describe 'with a valid Google token and valid location' do
+    it 'creates a new location' do
+      VCR.use_cassette('add_location_mutation_spec/valid_location') do
+        mutation = add_valid_location_mutation
 
-    mutation = add_location_mutation(@existing_user)
+        post '/graphql', params: {
+                           google_token: @existing_user.google_token,
+                           query: mutation
+                         }
 
-    post '/graphql', params: { query: mutation }
+        json = JSON.parse(response.body, symbolize_names: true)
 
-    json = JSON.parse(response.body, symbolize_names: true)
+        data = json[:data][:addLocation]
 
-    data = json[:data][:addAddress]
-
-    expect(data[:message]).to eq('Address successfully added')
+        expect(data[:message]).to eq('Location successfully added')
+      end
+    end
   end
 
-  it 'does not add a location if an invald Google token is passed in' do
-    user = double(
-      first_name: 'Bob',
-      last_name: 'Smith II',
-      email: 'bobsmithii@bs.com',
-      google_token: 'thisisthesecondbesttoken',
-    )
+  describe 'with a valid Google token and invalid location' do
+    it 'does not create a new location' do
+      VCR.use_cassette('add_location_mutation_spec/invalid_location') do
+        mutation = add_invalid_location_mutation
 
-    mutation = add_location_mutation(user)
-
-    post '/graphql', params: { query: mutation }
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    data = json[:data][:addAddress]
-
-    expect(data[:message]).to eq('No user found with that Google token')
+        expect {
+          post '/graphql', params: {
+                             google_token: @existing_user.google_token,
+                             query: mutation
+                           }
+        }.to raise_error(RuntimeError, 'Invalid address entered')
+      end
+    end
   end
 
-  def add_location_mutation(user)
+  describe 'with an invalid Google token' do
+    it 'does not create a new location' do
+      user = double(
+        first_name: 'Bob',
+        last_name: 'Smith II',
+        email: 'bobsmithii@bs.com',
+        google_token: 'thisisthesecondbesttoken',
+      )
+
+      mutation = add_valid_location_mutation
+
+      post '/graphql', params: {
+                         google_token: user.google_token,
+                         query: mutation
+                       }
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      data = json[:data][:addLocation]
+      error_message = json[:errors][0][:message]
+
+      expect(data).to be_nil
+      expect(error_message).to eq('Unauthorized - a valid google_token query parameter is required')
+    end
+  end
+
+  def add_valid_location_mutation
     "mutation {
-      addAddress(
-        googleToken: \"#{user.google_token}\"
+      addLocation(
+        location: {
+          streetAddress: \"1331 17th Street\",
+          city: \"Denver\",
+          state: \"CO\",
+          zipCode: \"80202\"
+        }
+      ) {
+        message
+      }
+    }"
+  end
+
+  def add_invalid_location_mutation
+    "mutation {
+      addLocation(
+        location: {
+          streetAddress: \"12345 Seeded Rye Bread Lane\",
+          city: \"Breadtown\",
+          state: \"OP\",
+          zipCode: \"12345\"
+        }
       ) {
         message
       }
