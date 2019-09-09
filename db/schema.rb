12f3e6279dd21cef93412e958fe67e3349d9079b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-09 13:30:38 -0600

    Change name of google_token column of User to token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_225333) do
+ActiveRecord::Schema.define(version: 2019_09_09_192849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,8 +66,8 @@ ActiveRecord::Schema.define(version: 2019_09_06_225333) do
     t.text "long_desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "google_token"
-    t.index ["google_token"], name: "index_users_on_google_token"
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token"
   end
 
   add_foreign_key "dogs", "users"
