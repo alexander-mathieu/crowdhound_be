013acd3105efbebfae66a234a60852e05c19c72b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-09-09 15:40:33 -0600

    Add destroyDog to GraphQL schema

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,10 +2,11 @@ module Types
   class MutationType < Types::BaseObject
     field :authenticate_user, mutation: Mutations::AuthenticateUser
     field :log_out_user, mutation: Mutations::LogOutUser
+    field :updateUser, mutation: Mutations::UpdateUser
 
     field :create_location, mutation: Mutations::CreateLocation
-    field :create_dog, mutation: Mutations::CreateDog
 
-    field :updateUser, mutation: Mutations::UpdateUser
+    field :create_dog, mutation: Mutations::CreateDog
+    field :destroy_dog, mutation: Mutations::DestroyDog
   end
 end
