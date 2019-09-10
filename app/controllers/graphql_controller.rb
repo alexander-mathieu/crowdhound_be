d585db408a7444dd7570156ccbf532642b756920
Alexandra Chakeres <alexchakeres@gmail.com>
2019-09-10 16:02:01 -0600

    Don't include current_chatkit_user in GraphQL context

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -5,7 +5,7 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       current_user: current_user,
-      current_chatkit_user: current_chatkit_user,
+      # current_chatkit_user: current_chatkit_user,
       file: params[:file]
     }
     result = CrowdhoundBeSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
@@ -23,13 +23,15 @@ class GraphqlController < ApplicationController
     @current_user ||= User.find_by(token: token)
   end
 
-  def current_chatkit_user
-    if current_user
-      ChatkitService(current_user).existing_chatkit_user
-    else
-      nil
-    end
-  end
+  # don't want to make external calls to chatkit in tests
+  
+  # def current_chatkit_user
+  #   if current_user
+  #     ChatkitService.new(current_user).existing_chatkit_user
+  #   else
+  #     nil
+  #   end
+  # end
 
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
